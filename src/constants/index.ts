bb64044350b6379c88993b0aba09d1be4edd6033
Krzysztof <krzysztofs012@outlook.com>
2021-03-26 23:50:35 +0100

    Changed dummyArticles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,7 +8,7 @@ export const dummyArticles = [{
   id: 1,
   title: 'Hello!',
   author: 'Chris',
-  description: `It's your first article in this pseudo blog.`,
+  content: `It's your first article in this pseudo blog.`,
   category: Category.TRAVELS,
   date: new Date(),
   minutesToRead: 0
